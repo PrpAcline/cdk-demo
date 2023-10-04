cacfb7b4fa7c9621996363d7384085b52d4eee70
Andrew Cline <acline@precisionplanting.com>
2023-10-04 09:56:51 -0500

    fix: syntax error in lambda file

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,22 +1,4 @@
 import fastifyLambda from "@fastify/aws-lambda";
 import app from "./app.js";
 
-// export const handler = fastifyLambda(app());
-
-export const handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello world",
-    }),
-  };
-};
-
-export const handler2 = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello world",
-    }),
-  };
-})
+export const handler = fastifyLambda(app());
