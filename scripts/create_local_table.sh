48f8fe7ec94c4cef90dba8ef7469f8609e30f8a6
Andrew Cline <acline@precisionplanting.com>
2023-10-04 12:09:01 -0500

    chore: fix create_local_table script

--- scripts/create_local_table.sh
+++ scripts/create_local_table.sh
@@ -8,4 +8,4 @@ aws dynamodb create-table --table-name fantastic-tacos \
     --attribute-definitions AttributeName=id,AttributeType=S \
     --key-schema AttributeName=id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
-    --endpoint-url http://dynamodb-local:8000 \
+    --endpoint-url http://localhost:8000 \
