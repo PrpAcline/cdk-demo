7bf9ecd51bdf9b300f256b145abc95a2621c2446
Andrew Cline <acline@precisionplanting.com>
2023-10-04 10:30:45 -0500

    feat: add initial GET routes

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,57 @@ type FastifyConfig = fastify.FastifyHttpOptions<
 const initApp = (config: FastifyConfig) => {
   const app = fastify(config);
 
-  app.get("/", async () => {
-    return { hello: "world" };
+  app.get("/up", async () => {
+    return { up: true };
+  });
+
+  app.get("/stores", async () => {
+    return {
+      type: "stores",
+      items: [
+        {
+          id: "1",
+          location: "London",
+        },
+        {
+          id: "2",
+          location: "Paris",
+        },
+        {
+          id: "3",
+          location: "New York",
+        },
+        {
+          id: "4",
+          location: "Tokyo",
+        },
+      ],
+    };
+  });
+
+  app.get("/stores/:id/menu", async (request) => {
+    const { id } = request.params as { id: string };
+
+    return {
+      type: "menu",
+      items: [
+        {
+          id: "1",
+          name: "Lava Taco",
+          price: 2.5,
+        },
+        {
+          id: "2",
+          name: "Lava Burrito",
+          price: 5,
+        },
+        {
+          id: "3",
+          name: "Lava Bowl",
+          price: 7.5,
+        },
+      ],
+    };
   });
 
   return app;
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,6 +1,6 @@
-import { describe, it, expect, afterEach, beforeEach } from "vitest";
+import { test, expect, afterEach, beforeEach } from "vitest";
 import { setupServer } from "msw/node";
-import setupApp from "../src/app";
+import initApp from "../src/app";
 
 const server = setupServer();
 
@@ -13,16 +13,72 @@ afterEach(() => {
   server.close();
 });
 
-describe("initial hello world route", () => {
-  it("should be true", async () => {
-    const app = setupApp({ logger: false });
+const app = initApp({ logger: false });
 
-    const response = await app.inject({
-      method: "GET",
-      url: "/",
-    });
+test("/up route that always returns 200", async () => {
+  const response = await app.inject({
+    method: "GET",
+    url: "/up",
+  });
 
-    expect(response.statusCode).toEqual(200);
-    expect(response.body).toEqual(JSON.stringify({ hello: "world" }));
+  expect(response.statusCode).toEqual(200);
+});
+
+test("/stores route that returns the list of stores", async () => {
+  const response = await app.inject({
+    method: "GET",
+    url: "/stores",
+  });
+
+  expect(response.statusCode).toEqual(200);
+  expect(response.json()).toEqual({
+    type: "stores",
+    items: [
+      {
+        id: "1",
+        location: "London",
+      },
+      {
+        id: "2",
+        location: "Paris",
+      },
+      {
+        id: "3",
+        location: "New York",
+      },
+      {
+        id: "4",
+        location: "Tokyo",
+      },
+    ],
+  });
+});
+
+test("/stores/:id/menu route that returns the list of menu items", async () => {
+  const response = await app.inject({
+    method: "GET",
+    url: "/stores/1/menu",
+  });
+
+  expect(response.statusCode).toEqual(200);
+  expect(response.json()).toEqual({
+    type: "menu",
+    items: [
+      {
+        id: "1",
+        name: "Lava Taco",
+        price: 2.5,
+      },
+      {
+        id: "2",
+        name: "Lava Burrito",
+        price: 5,
+      },
+      {
+        id: "3",
+        name: "Lava Bowl",
+        price: 7.5,
+      },
+    ],
   });
 });
