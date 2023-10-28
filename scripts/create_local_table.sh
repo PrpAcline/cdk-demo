9fa91b211a3dc2d0a2534c65c431f64dc719a28a
Andrew Cline <acline@precisionplanting.com>
2023-10-27 19:44:51 -0500

    feat: add create local table

--- scripts/create_local_table.sh
+++ scripts/create_local_table.sh
@@ -4,7 +4,9 @@ export AWS_ACCESS_KEY_ID=DUMMYIDEXAMPLE
 export AWS_SECRET_ACCESS_KEY=DUMMYEXAMPLEKEY
 export AWS_REGION=us-east-2
 
-aws dynamodb create-table --table-name fantastic-tacos \
+
+
+aws dynamodb create-table --table-name $(TABLE_NAME) \
     --attribute-definitions AttributeName=pk,AttributeType=S AttributeName=sk,AttributeType=S \
     --key-schema AttributeName=pk,KeyType=HASH AttributeName=sk,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
